73782c28c702fc561632e919d028585ed2851ad5
sebas <sebaslg96@gmail.com>
2024-09-30 08:34:38 -0500

    agregando metodos en admin controller y adminModel

--- models/adminsModel.php
+++ models/adminsModel.php
@@ -23,16 +23,15 @@ class AdminsModel{
         return $admins;
     }
 
-    ///codigo ejemplo
-    //function agregarAprendiz($numeroDoc, $nombreAprendiz, $apellidoAprendiz, $generoAprendiz, $fechaNacimiento, $telefono, $email){
-    //    $consulta = "INSERT INTO aprendices (num_Doc_Apr , nombre_Apr, apellido_Apr, genero_Apr, fecha_Nacimiento_Apr, telefono_Apr, correo_Apr) VALUES (?, ?, ?, ?, ?, ?, ?)";
-    //    return $this->db->efectuarConsulta($consulta, [$numeroDoc, $nombreAprendiz, $apellidoAprendiz, $generoAprendiz, $fechaNacimiento, $telefono, $email], 'sssssss');
-    //}
-//
-    //function actualizarAprendiz($numeroDoc, $nombreAprendiz, $apellidoAprendiz, $generoAprendiz, $fechaNacimiento, $telefono, $email){
-    //    $sql = "UPDATE aprendices SET nombre_Apr = ?, apellido_Apr = ?, genero_Apr = ?, fecha_Nacimiento_Apr = ?, telefono_Apr = ?, correo_Apr = ? WHERE num_Doc_Apr = ?";
-    //    return $this->db->efectuarConsulta($sql, [$nombreAprendiz, $apellidoAprendiz, $generoAprendiz, $fechaNacimiento, $telefono, $email, $numeroDoc], 'sssssss');
-    //}
+    function agregarAdmin($usuario, $pass, $email, $fnombre, $apellido){
+        $consulta = "INSERT INTO `admin` (usuario_Adm, pass_Adm, correo_Adm, nombre_Adm, apellido_Adm) VALUES (?, ?, ?, ?, ?)";
+        return $this->db->efectuarConsulta($consulta, [$usuario, $pass, $email, $fnombre, $apellido], 'sssss');
+    }
+
+    function actualizarAdmin($usuario, $pass, $email, $fnombre, $apellido, $idAdmin){
+        $consulta = "UPDATE `admin` SET usuario_Adm = ?, pass_Adm = ?, correo_Adm = ?, nombre_Adm = ?, apellido_Adm = ? WHERE id_Adm = ?";
+        return $this->db->efectuarConsulta($consulta, [$usuario, $pass, $email, $fnombre, $apellido], 'ssssss');
+    }
 }
 
 ?>
